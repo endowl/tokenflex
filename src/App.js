a0ff2524a7523821c2910f0bca1e0e70fbb8904d
bgok <ken@citizenhodler.com>
2021-10-02 16:23:50 -0700

    nft-image component (WIP)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Main from './components/main';
 import './App.css';
+import NftImage from "./components/nft-image";
 
 function App() {
-  return (
-    <div className="App">
-      <Main />
-    </div>
-  );
+    return (
+        <div className="App">
+            <NftImage contractAddress="0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb" tokenId="3100"
+                      style={{width: '150px', height: '150px'}}/>
+            <Main/>
+        </div>
+    );
 }
 
 export default App;
