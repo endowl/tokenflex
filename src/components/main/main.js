3cab88f8ed22c9d3ff75837355eb169baf740a0d
Aaron Anderson <aaronlynanderson@gmail.com>
2021-10-02 14:25:29 -0400

    Wireframe divs

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -3,7 +3,27 @@ import './main.css';
 function Main() {
   return (
     <div className="main">
-      Main
+
+      <div>
+        <div>
+          Seal
+        </div>
+        <div>
+          Logo
+        </div>
+        <div>
+          Connect
+        </div>
+      </div>
+
+      <div>
+        Landing
+      </div>
+
+      <div>
+        Product
+      </div>
+      
     </div>
   );
 }
